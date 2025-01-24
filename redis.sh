bd6259ed88ecdb2e4b730f0717837f52b8cfd88c
rakesh <rakeshreddy@gmail.com>
2025-01-24 18:13:34 +0530

    added redis.sh

--- redis.sh
+++ redis.sh
@@ -20,6 +20,8 @@ else
 echo  -e " $r $2 ... failure $n"
 fi  
 }
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+yum module enable redis:remi-6.2 -y
 yum install redis -y &>> $logfile
 
 validate $? "installing redis"
