9f615cdeb7e4607f5ab9eec24f88aa1ec578da70
rakesh <rakeshreddy@gmail.com>
2025-01-29 15:22:18 +0530

    modified create-ec2.sh

--- create-ec2.sh
+++ create-ec2.sh
@@ -4,7 +4,8 @@ servers=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shippin
 instance_type=""
 imageid="ami-0b4f379183e5706b9" 
 securityid="sg-0dc7448f0fa6926f1"
-
+domain_name="joindevops.store"
+hostedzoneid="Z0391488M5DNTAYOTVFM"
 
 for i in ${servers[@]}
 do
@@ -14,7 +15,26 @@ instance_type="t3.micro"
 else
 instance_type="t2.micro"
 fi
-aws ec2 run-instances --image-id $imageid --instance-type $instance_type --security-group-ids $securityid --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="$i"}]"  | jq -r ".Instances[0].PrivateIpAddress"
-
+privateip=aws ec2 run-instances --image-id $imageid --instance-type $instance_type --security-group-ids $securityid --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="$i"}]"  | jq -r ".Instances[0].PrivateIpAddress"
+echo "$i private ip address $privateip"
+aws route53  --hosted-zone-id $hostedzoneid --change-batch 
+{
+    "Comment": "Update record to add new CNAME record",
+    "Changes": [
+        {
+            "Action": "CREATE",
+            "ResourceRecordSet": {
+                "Name": "$i.$domain_name",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                    {
+                        "Value": "$privateip"
+                    }
+                ]
+            }
+        }
+    ]
+} 
 
 done
