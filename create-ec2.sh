592ed69011f84decdfa7d2599d0274faa2a17b0d
rakesh <rakeshreddy@gmail.com>
2025-02-02 17:52:26 +0530

    modified create-ec2.sh and cart.service

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,6 +1,6 @@
 
 
-servers=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping" "payment" "dispatch" "web")
+servers=$@
 instance_type=""
 imageid="ami-0b4f379183e5706b9" 
 securityid="sg-0dc7448f0fa6926f1"
@@ -8,7 +8,7 @@ domain_name=joindevops.store
 hostedzoneid=Z0391488M5DNTAYOTVFM
 
 
-for i in ${servers[@]}
+for i in $@
 do
 if [[ $i == "mongodb" || $i == "mysql" ]]
 then
@@ -16,6 +16,7 @@ instance_type="t3.micro"
 else
 instance_type="t2.micro"
 fi
+if [ ]
 privateip=$(aws ec2 run-instances --image-id $imageid --instance-type $instance_type --security-group-ids $securityid --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="$i"}]"  | jq -r ".Instances[0].PrivateIpAddress")
 echo "$i private ip address $privateip"
 aws route53 change-resource-record-sets --hosted-zone-id $hostedzoneid --change-batch '
